849ed6701b65995fe143b6b53152cdcb8d8c7411
Robert Zhang <louirobert@gmail.com>
2024-08-03 11:58:05 +0800

    Add SampleAppWithListener

---
+++ src/SampleAppWithListener/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SampleAppWithListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main starts.");
+
+            Trace.WriteLine("some line");
+            Trace.TraceInformation("some info");
+            Trace.TraceWarning("some warning");
+            Trace.TraceError("some error");
+
+            Console.WriteLine("Main ends.");
+        }
+    }
+}
