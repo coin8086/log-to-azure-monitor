3293742e132597b0ee7aac5f0402945fa5e107d6
Robert Zhang <louirobert@gmail.com>
2024-08-03 14:52:58 +0800

    SampleAppWithListener: Keep output trace info until Ctrl-C

--- src/SampleAppWithListener/Program.cs
+++ src/SampleAppWithListener/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SampleAppWithListener
 {
@@ -7,14 +9,33 @@ namespace SampleAppWithListener
     {
         static void Main(string[] args)
         {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var end = new ManualResetEvent(false);
+
+            Console.CancelKeyPress += (sender, evt) => {
+                cts.Cancel();
+                end.WaitOne();
+            };
+
             Console.WriteLine("Main starts.");
 
-            Trace.WriteLine("some line");
-            Trace.TraceInformation("some info");
-            Trace.TraceWarning("some warning");
-            Trace.TraceError("some error");
+            while (!token.IsCancellationRequested)
+            {
+                Trace.WriteLine("some line");
+                Trace.TraceInformation("some info");
+                Trace.TraceWarning("some warning");
+                Trace.TraceError("some error");
+
+                try
+                {
+                    Task.Delay(2000).Wait(token);
+                }
+                catch (OperationCanceledException) { }
+            }
 
             Console.WriteLine("Main ends.");
+            end.Set();
         }
     }
 }
