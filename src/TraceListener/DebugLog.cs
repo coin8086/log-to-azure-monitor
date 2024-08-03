742602f917390df2b34f2a6343392d489787b918
Robert Zhang <louirobert@gmail.com>
2024-08-03 11:01:43 +0800

    Write log to ETW when it's available.

--- src/TraceListener/DebugLog.cs
+++ src/TraceListener/DebugLog.cs
@@ -1,27 +1,69 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace RzWork.AzureMonitor
 {
     internal static class DebugLog
     {
+        enum LogLevel
+        {
+            Info,
+            Warning,
+            Error,
+        }
+
+        private static EventLogEntryType ToEventLogEntryType(LogLevel level)
+        {
+            EventLogEntryType t = EventLogEntryType.Information;
+            switch (level)
+            {
+                case LogLevel.Warning:
+                    t = EventLogEntryType.Warning;
+                    break;
+                case LogLevel.Error:
+                    t = EventLogEntryType.Error;
+                    break;
+            }
+            return t;
+        }
+
+        private static string EtwSource = typeof(DebugLog).Namespace;
+
+        private static string EtwLog = typeof(DebugLog).FullName;
+
+        static DebugLog()
+        {
+            if (!EventLog.Exists(EtwSource))
+            {
+                EventLog.CreateEventSource(EtwSource, EtwLog);
+            }
+        }
+
         public static void WriteInfo<T>(string msg)
         {
-            Write("Info", typeof(T).FullName, msg);
+            Write(LogLevel.Info, typeof(T).FullName, msg);
         }
 
         public static void WriteWarning<T>(string msg)
         {
-            Write("Warn", typeof(T).FullName, msg);
+            Write(LogLevel.Warning, typeof(T).FullName, msg);
         }
 
         public static void WriteError<T>(string msg)
         {
-            Write("Error", typeof(T).FullName, msg);
+            Write(LogLevel.Error, typeof(T).FullName, msg);
         }
 
-        private static void Write(string level, string category, string msg)
+        private static void Write(LogLevel level, string category, string msg)
         {
-            Console.Error.WriteLine($"LA {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff} {level} {category} {msg}");
+            if (EventLog.Exists(EtwSource))
+            {
+                EventLog.WriteEntry(EtwSource, $"{category} {msg}", ToEventLogEntryType(level));
+            }
+            else
+            {
+                Console.Error.WriteLine($"{EtwLog} {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff} {level} {category} {msg}");
+            }
         }
     }
 }
